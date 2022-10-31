62fb333ea599f269ee9a580c82b95a140532eae2
Darrin Grove <dgrove@truefit.io>
2022-10-31 18:14:41 -0400

    Initial Commit

---
+++ BodyTrackingDemo/BodyTrackingDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  BodyTrackingDemoApp.swift
+//  BodyTrackingDemo
+//
+//  Created by Darrin Grove on 10/31/22.
+//
+
+import SwiftUI
+
+@main
+struct BodyTrackingDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
